153db873467938d171e63d7e49634d256ad2476e
Julien Greffe <jgreffe@cloudbees.com>
2022-10-20 14:24:58 +0200

    Additional test to use standard selenium API

--- src/test/java/sandbox/AbstractDriverTest.java
+++ src/test/java/sandbox/AbstractDriverTest.java
@@ -2,6 +2,7 @@ package sandbox;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import io.github.sukgu.Shadow;
@@ -22,15 +23,39 @@ public abstract class AbstractDriverTest {
         DRIVER.quit();
     }
 
+    /**
+     * Standard input
+     *
+     * @throws Exception
+     */
     @Test
     public void input() throws Exception {
         SHADOW.findElement("input").sendKeys("test");
     }
 
+    /**
+     * Use automation plugin to traverse web component shadow dom
+     *
+     * @throws Exception
+     */
     @Test
     public void mwc_textfield_shadow() throws Exception {
         WebElement element = SHADOW.findElement("mwc-textfield");
         element.click();
         element.sendKeys("test");
+    }
+
+    /**
+     * Use standard selenium API to traverse web component shadow dom
+     *
+     * @throws Exception
+     */
+    @Test
+    public void mwc_textfield_selenium_shadow() throws Exception {
+        WebElement customTextField = DRIVER.findElement(By.cssSelector("custom-text-field"));
+        WebElement mwcTextfield = customTextField.getShadowRoot().findElement(By.cssSelector("mwc-textfield"));
+        WebElement input = mwcTextfield.getShadowRoot().findElement(By.cssSelector("input"));
+        input.click();
+        input.sendKeys("test");
     }
 }
