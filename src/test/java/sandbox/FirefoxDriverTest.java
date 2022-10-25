b62a351c63f4d5f5d078a16daf03fd3e61baf215
Julien Greffe <jgreffe@cloudbees.com>
2022-10-25 18:28:28 +0200

    Trace log on geckodriver

--- src/test/java/sandbox/FirefoxDriverTest.java
+++ src/test/java/sandbox/FirefoxDriverTest.java
@@ -2,11 +2,15 @@ package sandbox;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class FirefoxDriverTest extends AbstractDriverTest {
 
     @BeforeAll
     public static void before() {
-        AbstractDriverTest.before(new FirefoxDriver());
+        FirefoxOptions options = new FirefoxOptions();
+        options.setLogLevel(FirefoxDriverLogLevel.TRACE);
+        AbstractDriverTest.before(new FirefoxDriver(options));
     }
 }
